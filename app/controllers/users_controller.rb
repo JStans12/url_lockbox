57e0be52a0026d8c727a32f2025b381a6cedc584
Joey Stansfield <joseph.r.stansfield@gmail.com>
2017-02-13 13:50:09 -0700

    sign up errors work and tested

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,8 @@ class UsersController < ApplicationController
       session[:user_id] = user.id
       redirect_to root_path
     else
+      error = user.errors.messages.first[1][0]
+      flash[:error] = error_key[error]
       redirect_to login_path
     end
   end
@@ -14,5 +16,10 @@ class UsersController < ApplicationController
 
     def user_params
       params.permit(:email, :password, :password_confirmation)
+    end
+
+    def error_key
+      {"has already been taken" => "email has already been taken",
+       "doesn't match Password" => "passwords don't match"}
     end
 end
--- spec/features/unauthenticated_user_can_create_an_accout_spec.rb
+++ spec/features/unauthenticated_user_can_create_an_accout_spec.rb
@@ -12,4 +12,30 @@ RSpec.describe "unauthenticated user" do
     expect(page).to have_content("Logged in as jim@example.com")
     expect(User.count).to eq(1)
   end
+
+  it "cant create an account with taken email" do
+    user = create(:user)
+
+    visit '/'
+    within('#sign-up') do
+      fill_in "email", with: user.email
+      fill_in "password", with: "password"
+      fill_in "password_confirmation", with: "password"
+      click_on "Sign Up"
+    end
+    expect(page).to have_content("email has already been taken")
+    expect(User.count).to eq(1)
+  end
+
+  it "cant create an account with unmatching passwords" do
+    visit '/'
+    within('#sign-up') do
+      fill_in "email", with: "jim@exapmle.com"
+      fill_in "password", with: "password"
+      fill_in "password_confirmation", with: "passwordz"
+      click_on "Sign Up"
+    end
+    expect(page).to have_content("passwords don't match")
+    expect(User.count).to eq(0)
+  end
 end
