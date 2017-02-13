39afe5720f351eae54a79df6a18908c9ed5c770a
Joey Stansfield <joseph.r.stansfield@gmail.com>
2017-02-13 13:14:38 -0700

    remove useless password fields

--- db/schema.rb
+++ db/schema.rb
@@ -27,10 +27,8 @@ ActiveRecord::Schema.define(version: 20170213201142) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email"
-    t.string   "password"
-    t.string   "password_confirmation"
-    t.datetime "created_at",            null: false
-    t.datetime "updated_at",            null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
     t.string   "password_digest"
   end
 
