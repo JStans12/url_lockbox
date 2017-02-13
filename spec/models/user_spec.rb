c6900af5f52b2c10638aa6272b2ecf2affe2da71
Joey Stansfield <joseph.r.stansfield@gmail.com>
2017-02-13 13:12:33 -0700

    user has secure password

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe "validations" do
     it { should validate_uniqueness_of :email }
+    it { should have_secure_password }
   end
 
   describe "relationships" do
