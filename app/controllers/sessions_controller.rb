97dec56700bb5e3647ed52f6946cd4584f35cae9
Joey Stansfield <joseph.r.stansfield@gmail.com>
2017-02-13 13:56:11 -0700

    login errors work and tested

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,10 +7,15 @@ class SessionsController < ApplicationController
     if user = User.find_by(email: params[:email])
       if user.authenticate(params[:password])
         session[:user_id] = user.id
+        flash[:success] = "logged in as #{user.email}"
         redirect_to root_path
       else
+        flash[:error] = "unsuccessful login attempt"
+        redirect_to root_path
       end
     else
+      flash[:error] = "unsuccessful login attempt"
+      redirect_to root_path
     end
   end
 end
--- spec/features/registered_user_can_login_spec.rb
+++ spec/features/registered_user_can_login_spec.rb
@@ -12,4 +12,28 @@ RSpec.describe "registered user" do
     end
     expect(page).to have_content("Logged in as #{user.email}")
   end
+
+  it "cant login with bad password" do
+    user = create(:user)
+
+    visit '/'
+    within('#login') do
+      fill_in "email", with: user.email
+      fill_in "password", with: "passwordz"
+      click_on "Login"
+    end
+    expect(page).to have_content("unsuccessful login attempt")
+  end
+
+  it "cant login with bad email" do
+    user = create(:user)
+
+    visit '/'
+    within('#login') do
+      fill_in "email", with: "stuff"
+      fill_in "password", with: "passwordz"
+      click_on "Login"
+    end
+    expect(page).to have_content("unsuccessful login attempt")
+  end
 end
