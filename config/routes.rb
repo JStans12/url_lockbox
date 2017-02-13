e9366bc4147d8fe2a2bd8165fb466b222556d5b3
Joey Stansfield <joseph.r.stansfield@gmail.com>
2017-02-13 13:06:35 -0700

    routes for create users and sessions added

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root to: "links#index"
 
   get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  post '/users', to: 'users#create'
 
   resources :links, only: [:index]
 
